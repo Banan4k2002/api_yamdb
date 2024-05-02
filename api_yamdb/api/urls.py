e5d765d1a419e8b7a01af30eb499b77a762ef4d5
Ana <Addamsus@gmail.com>
2024-05-02 15:58:56 +0300

    wip

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,21 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+
+
+v1_router = DefaultRouter()
+
+v1_router.register(r'users', UsersViewSet)
+v1_router.register(r'genres', GenresViewSet, basename='genres')
+v1_router.register(r'categiries', CategoryViewSet, basename='categiries')
+v1_router.register(r'titles', TitleViewSet, basename='titles')
+v1_router.register(
+    r'titles/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments'
+)
+v1_router.register(
+    r'titles/(?P<post_id>\d+)/reviews', ReviewViewSet, basename=' '
+)
+
+urlpatterns = [
+    path('v1/', include(v1_router.urls)),
+]
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from datetime import timedelta
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,6 +22,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    # 'drf_yasg',
+    # 'accounts',
 ]
 
 MIDDLEWARE = [
@@ -101,3 +106,22 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=14),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
