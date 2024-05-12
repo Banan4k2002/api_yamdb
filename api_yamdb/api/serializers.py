1842a53570b55733ce50be2c6d32bda6fa7cb53f
Алексей Ларионов <lesha.larionov.2002@gmail.com>
2024-05-12 11:47:21 +0300

    Update to PEP8

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -2,7 +2,6 @@ import re
 
 from django.contrib.auth import get_user_model
 from django.db.models import Avg
-
 from rest_framework import serializers
 from rest_framework.validators import ValidationError
 
