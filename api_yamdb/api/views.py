c201e0e16cf2cdfad81994e7c58de2ff56b5906e
texikator <wiktot@mail.ru>
2024-05-13 20:14:30 +0300

    create & partial_update returned

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -52,7 +52,8 @@ class GenreViewSet(CreateDestroyListViewSet):
 class TitleViewSet(ModelViewSet):
     """Вьюсет для произведений."""
 
-    queryset = Title.objects.all()
+    queryset = Title.objects.annotate(
+        rating=Round(Avg('reviews__score')))
     serializer_class = TitleSerializer
     filter_backends = (DjangoFilterBackend,)
     permission_classes = (
@@ -61,26 +62,27 @@ class TitleViewSet(ModelViewSet):
     )
     filterset_class = TitleFilter
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        queryset = queryset.annotate(
-            rating=Round(Avg('reviews__score')))
-        return queryset
-
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update']:
             return TitleCreateUpdateSerializer
         return TitleSerializer
 
-    def perform_create(self, serializer):
-        obj = serializer.save()
-        serializer = TitleSerializer(obj)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            obj = serializer.save()
+            serializer = TitleSerializer(obj)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def perform_update(self, serializer):
-        obj = serializer.save()
-        serializer = TitleSerializer(obj)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        if serializer.is_valid():
+            obj = serializer.save()
+            serializer = TitleSerializer(obj)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
